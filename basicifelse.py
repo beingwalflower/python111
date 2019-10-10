2b2711936b0d460dfc42e0c191042fe39272b7eb
beingwalflower <52874469+beingwalflower@users.noreply.github.com>
2019-10-10 08:59:17 +0530

    Update basicifelse.py

--- basicifelse.py
+++ basicifelse.py
@@ -1,6 +1,9 @@
-a=5
-b=6
-if(a>b):
-    print("{0} is smaller than {1}".format(b,a))
-else:
-    print("{0} is smaller than {1}".format(a,b))
+def greater(int(a),int(b)):
+    if(a>b):
+        print("{0} is smaller than {1}".format(b,a))
+    else:
+     print("{0} is smaller than {1}".format(a,b))
+
+x=input('enter number')
+y=input('enter number')
+greater(int(x),int(y))
